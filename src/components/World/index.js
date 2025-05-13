4ed979f45b218730725b2539e01cdb128e1aa4e0
Yigitalp Demirci <ahmetdemirci1719@gmail.com>
2025-05-13 22:08:39 +0300

    feat: add iframe to World endpoint and minor style adjustments

--- src/components/World/index.js
+++ src/components/World/index.js
@@ -5,7 +5,19 @@ const World = () => {
   return (
     <div className="world-page">
       <h1>World</h1>
-      <p>This is the world page of my portfolio website.</p>
+      <p>
+        You can access our demo below.<br />
+        To orbit around the planet, hold down the left mouse button.<br />
+        Zoom in to descend to the surface, then use the WASD keys to move and Spacebar to jump.<br />
+        At any time, you may zoom out to return to the planetary view.
+      </p>
+      <div className="iframe-container">
+        <iframe
+          src="http://localhost:8000"
+          title="Demo"
+          className="embedded-iframe"
+        />
+      </div>
     </div>
   );
 };
